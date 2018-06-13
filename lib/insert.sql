ea8b20d1281b801ce147e49af098d08735b8089f
bkrigel <benjamin.krigel@gmail.com>
2018-06-13 13:32:25 -0400

    good exercise

---
+++ lib/insert.sql
@@ -0,0 +1,16 @@
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Mr. Chocolate", 2, "M", "brown", "hungry", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Rowdy", 3, "M", "black", "energetic", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Tabitha", 10, "F", "grizzly", "sleepy", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Sergeant Brown", 7, "M", "black", "serious", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Melissa", 7, "F", "koala", "scared", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Grinch", 5, "M", "polar", "vengeful", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES ("Wendy", 200, "F", "teddy", "fluffy", 0);
+INSERT INTO bears (name, age, gender, color, temperament, alive)
+    VALUES (NULL, 4, "F", "grizzly", "agitated", 1);
